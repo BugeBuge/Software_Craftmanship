18876aabe9fd59df90962e594576ae95314d9120
baptiste <baptiste.monfray@epita.fr>
2021-07-07 22:00:12 +0200

    [ADD][Services]: add getMissionsByKeyword method

--- src/main/java/socra/project/services/TaskServices.java
+++ src/main/java/socra/project/services/TaskServices.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import socra.project.elements.Task;
 import socra.project.repositories.TaskRepository;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -31,5 +33,35 @@ public class TaskServices {
         else {
             throw new IllegalStateException("Mission with id " + taskId + " does not exist");
         }
+    }
+
+    public List<Task> getMissionsByKeyword(List<String> keywords) {
+        List<Task> missions = taskRepository.findAllByOrderByIdDesc();
+        int size = missions.size();
+        int[] scores = new int[size];
+        Arrays.fill(scores, 0);
+        List<Task> missionsFiltered = new ArrayList<>();
+        for (Task mission : missions) {
+            String description = mission.getMission();
+            for (String keyword : keywords) {
+                if (description.toLowerCase().contains(keyword.toLowerCase())){
+                    int position = missions.indexOf(mission);
+                    scores[position]++;
+                }
+            }
+        }
+        int nbKeywords = keywords.size();
+        while (nbKeywords > 0)
+        {
+            int pos = 0;
+            while (pos < scores.length){
+                if (scores[pos] == nbKeywords) {
+                    missionsFiltered.add(missions.get(pos));
+                }
+                pos++;
+            }
+            nbKeywords--;
+        }
+        return missionsFiltered;
     }
 }
