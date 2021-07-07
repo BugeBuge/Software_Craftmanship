ec3e33a5021d9825ff3dac13c9ebd8419f9a1fb2
wartad <corentin.keloghlanian@gmail.com>
2021-07-07 20:49:27 +0200

    [ADD][Services] : get task method in services

--- src/main/java/socra/project/services/TaskServices.java
+++ src/main/java/socra/project/services/TaskServices.java
@@ -1,4 +1,22 @@
 package socra.project.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import socra.project.elements.Task;
+import socra.project.repositories.TaskRepository;
+
+import java.util.List;
+
+@Service
 public class TaskServices {
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskServices(TaskRepository taskRepository){
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getMissions(){
+        return taskRepository.findAllByOrderByIdDesc();
+    }
 }
