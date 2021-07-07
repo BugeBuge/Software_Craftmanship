9914607058761c7a5547835f68f2479d28867760
erwan <erwansalle@gmail.com>
2021-07-07 22:32:28 +0200

    [UPDATE][Services] : add method to put a task

--- src/main/java/socra/project/services/TaskServices.java
+++ src/main/java/socra/project/services/TaskServices.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import socra.project.elements.Task;
 import socra.project.repositories.TaskRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -22,6 +23,22 @@ public class TaskServices {
 
     public void addNewTask(Task task) {
         taskRepository.save(task);
+    }
+
+    @Transactional
+    public void updateTask(Long taskId, Task task) {
+        Task taskInDb = taskRepository.findById(taskId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Mission with id " + taskId + " does not exist"
+                ));
+        taskInDb.setLieu(task.getLieu());
+        taskInDb.setDurée(task.getDurée());
+        taskInDb.setTarif(task.getTarif());
+        taskInDb.setTélétravail(task.getTélétravail());
+        taskInDb.setDébut(task.getDébut());
+        taskInDb.setPoste(task.getPoste());
+        taskInDb.setContexte(task.getContexte());
+        taskInDb.setMission(task.getMission());
     }
 
     public void deleteTask(Long taskId) {
