0362fb718173b88304d37c81581e1c8e3151f902
Mathieu BILLY <mathieu.billy@epita.fr>
2021-07-07 23:15:50 +0200

    [UPDATE][TaskServices] : finished the export function

--- src/main/java/socra/project/services/TaskServices.java
+++ src/main/java/socra/project/services/TaskServices.java
@@ -1,10 +1,15 @@
 package socra.project.services;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import socra.project.elements.Task;
 import socra.project.repositories.TaskRepository;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.List;
 
 @Service
@@ -39,6 +44,32 @@ public class TaskServices {
                         "Task with id " + taskId + " does not exist"
                 ));
         //TO DO
+
+        Document document = new Document();
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream("task.pdf"));
+        } catch (DocumentException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+
+        try {
+            document.add(new Paragraph("Id : " + taskId));
+            document.add(new Paragraph("Lieu : " + taskInDb.getLieu()));
+            document.add(new Paragraph("Durée : " + taskInDb.getDurée()));
+            document.add(new Paragraph("Tarif : " + taskInDb.getTarif()));
+            document.add(new Paragraph("Télétravail : " + taskInDb.getTélétravail()));
+            document.add(new Paragraph("Début : " + taskInDb.getDébut()));
+            document.add(new Paragraph("Poste : " + taskInDb.getPoste()));
+            document.add(new Paragraph("Contexte : " + taskInDb.getContexte()));
+            document.add(new Paragraph("Mission : " + taskInDb.getMission()));
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+
+        document.close();
         return taskInDb;
     }
 }
